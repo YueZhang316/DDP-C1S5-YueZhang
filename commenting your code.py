80de05eed1d144cea953298b67e08c6a478bc2db
YueZhang316 <156191944+YueZhang316@users.noreply.github.com>
2024-01-14 20:18:45 +0000

    Add files via upload

---
+++ commenting your code.py
@@ -0,0 +1,10 @@
+#This is a single-line comment
+print("Hello, World!")
+
+#This is a single-line comment
+#extending to the second line
+'''
+This is another way to write
+multi-line comments
+'''
+print("Hello, World!")
