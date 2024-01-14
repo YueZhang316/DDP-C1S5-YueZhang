726bc599f2bf47cac8a7a0d35cf3d2efcb15b631
YueZhang316 <156191944+YueZhang316@users.noreply.github.com>
2024-01-14 21:37:03 +0000

    Add files via upload

---
+++ Creating Strings.py
@@ -0,0 +1,22 @@
+#Creating Strings
+
+#Concetenating two strings
+greeting = "Hello"
+name = "Zoey"
+message = greeting + "," + name + "!"
+print(message)
+
+#Repeating a string
+laugh = "Ha"
+repeat_laugh = laugh * 3
+print(repeat_laugh)
+
+#Accessing a character in a string
+word = "Python"
+letter = word[0]
+print(letter)
+
+#Slicing a string
+word = "Hello, World!"
+sub_word = word[7 : 13]
+print(sub_word)
