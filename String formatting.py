1bffe18435d6ccb338355185ed6066949f09eb0a
YueZhang316 <156191944+YueZhang316@users.noreply.github.com>
2024-01-21 23:40:21 +0000

    Add files via upload

---
+++ String formatting.py
@@ -0,0 +1,26 @@
+name = "Zoey"
+age = 23
+message = "Hello, %s. You are %d years old." % (name, age)
+print(message)
+
+name = "Zoe"
+age = 24
+message = "Hello, {}. You are {} years old." .format(name,age)
+print(message)
+
+name = "Z"
+age = 25
+message = f"Hello, {name}. You are {age} years old."
+print(message)
+
+text = "Hello\nWorld"
+print(text)
+
+text = "Hello\tWorld"
+print(text)
+
+path = "C:\\Users\\Username"
+print(path)
+
+quote = "He said, \"Python is awesome!\""
+print(quote)
